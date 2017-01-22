b66bccec8ca82b777851aed2fd9f26fc18b3a1b1
Hendrik <test>
2017-01-22 12:33:24 +0100

    1

--- Trinkspiel/models.py
+++ Trinkspiel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class game(models.Model):
+
+
+class player(models.Model):
+    name = models.CharField(max_length=100)
+    sip = models.IntegerField
+    shots = models.IntegerField
+    # game = game
